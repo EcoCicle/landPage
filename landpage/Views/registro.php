f0d958ce021ad7bb1d3e5d894a0b64119368eb01
Arthur Ferreira Nascimento <arthurferreira1417@gmail.com>
2025-06-30 01:23:21 -0300

    FEAT: adicionando configurações e exluindo arquivos php

--- landpage/Views/registro.php
+++
@@ -1,9 +0,0 @@
-<form action="../controller/RegistroController.php" method="POST">
-    <input type="hidden" name="tipo" value="consumidor">
-    <input type="email" name="email" placeholder="E-mail" required>
-    <input type="password" name="senha" placeholder="Senha" required>
-    <button type="submit">Cadastrar</button>
-    <?php if (isset($_GET['erro'])): ?>
-        <div class="erro"><?php echo htmlspecialchars($_GET['erro']); ?></div>
-    <?php endif; ?>
-</form>
