3cd6fff02dce6aacc806fee8b946d12e8b351627
Arthur Ferreira Nascimento <arthurferreira1417@gmail.com>
2025-06-30 19:43:13 -0300

    FEAT: criando service para conexcão com o banco

---
+++ landpage/Services/SupabaseService.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+public class SupabaseService
+{
+    private readonly HttpClient _client;
+
+    public SupabaseService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+    {
+        _client = httpClientFactory.CreateClient();
+        _client.BaseAddress = new Uri(configuration["Supabase:Url"]);
+        _client.DefaultRequestHeaders.Clear();
+        _client.DefaultRequestHeaders.Add("apikey", configuration["Supabase:ApiKey"]);
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", configuration["Supabase:ApiKey"]);
+        _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+    public async Task<string> GetUsersAsync()
+    {
+        var response = await _client.GetAsync("usuários");
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+}
