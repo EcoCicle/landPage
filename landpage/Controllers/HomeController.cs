5313f6327adf724f1fb7ad70568477689a9bead2
Vital <luizhenrique.vital9@gmail.com>
2025-07-02 22:57:56 -0300

    Feat: adicionando funcionalidade a camêra e estilizando a imagem da pagina Perfil

--- landpage/Controllers/HomeController.cs
+++ landpage/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace WebApplication1.Controllers;
 
-[Authorize]
+// [Authorize]
 public class HomeController : Controller{
     private readonly SupabaseService _supabaseService;
 
